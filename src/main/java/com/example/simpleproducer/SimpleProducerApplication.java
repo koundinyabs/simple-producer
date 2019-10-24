8d9aa2afe5c2591459df0d7146eae8dc9d1ac0b3
Koundinya Srinivasarao <ksrinivasarao@pivotal.io>
2019-10-24 02:18:46 -0400

    first commit

---
+++ src/main/java/com/example/simpleproducer/SimpleProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleProducerApplication.class, args);
+	}
+
+}
